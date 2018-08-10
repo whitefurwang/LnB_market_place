a632dd77031d5988679595edbaebe3aa5b152e8d
Whitefur Wang <whitefur.wang@gmail.com>
2018-08-10 19:14:28 +0800

    Lots of update
    1. fixed modal bug of couldn't update when using browser prev/next page
    2. add navigation and related events
    3. fixed list th tag click bug

--- resources/assets/js/components/List.vue
+++ resources/assets/js/components/List.vue
@@ -2,35 +2,35 @@
   <table v-if="now_list && now_list.length" class='table table-hover'>
     <thead>
       <tr>
-        <th class="sort" ref='apr' @click='sort_items("apr")'>
+        <th class="sort" data-sort='apr' @click='sort_items'>
           <span>年利率</span>
           <div class="sort-icons">
             <i class='icon-sort-up'></i>
             <i class='icon-sort-down'></i>
           </div>
         </th>
-        <th class="sort" ref='period' @click='sort_items("period")'>
+        <th class="sort" data-sort='period' @click='sort_items'>
           <span>期數</span>
           <div class="sort-icons">
             <i class='icon-sort-up'></i>
             <i class='icon-sort-down'></i>
           </div>
         </th>
-        <th class="sort" ref='purpose' @click='sort_items("purpose")'>
+        <th class="sort" data-sort='purpose' @click='sort_items'>
           <span>貸款目的</span>
           <div class="sort-icons">
             <i class='icon-sort-up'></i>
             <i class='icon-sort-down'></i>
           </div>
         </th>
-        <th class="sort" ref='pgr' @click='sort_items("pgr")'>
+        <th class="sort" data-sort='pgr' @click='sort_items'>
           <span>保護比例</span>
           <div class="sort-icons">
             <i class='icon-sort-up'></i>
             <i class='icon-sort-down'></i>
           </div>
         </th>
-        <th class="sort" ref='expire_at' @click='sort_items("expire_at")'>
+        <th class="sort" data-sort='expire_at' @click='sort_items'>
           <span>下架倒數</span>
           <div class="sort-icons">
             <i class='icon-sort-up'></i>
@@ -49,7 +49,12 @@
         data-toggle="modal"
         data-target="#modal"
       >
-        <td class='align-middle'>{{ item.data.loan_detail.credit_level }} {{ item.data.loan_detail.apr.toFixed(2) }}%</td>
+        <td class='align-middle'>
+          <span class='credit_level' v-bind:class='{
+            credit_level_a: /^a/i.test(item.data.loan_detail.credit_level),
+            credit_level_b: /^b/i.test(item.data.loan_detail.credit_level),
+            credit_level_c: /^c/i.test(item.data.loan_detail.credit_level)
+          }'>{{ item.data.loan_detail.credit_level }}</span> {{ item.data.loan_detail.apr.toFixed(2) }}%</td>
         <td class='align-middle'>{{ item.data.loan_detail.period }}期</td>
         <td class='align-middle'>{{ item.data.loan_detail.purpose }}</td>
         <td class='align-middle'>{{ item.data.loan_detail.pgr.toFixed(2) }}%</td>
@@ -70,7 +75,8 @@
   export default {
     props: [
       'now_list',
-      'sort_items'
+      'sort_items',
+      'order'
     ]
   }
 </script>
--- resources/assets/js/components/MarketPlace.vue
+++ resources/assets/js/components/MarketPlace.vue
@@ -4,20 +4,58 @@
       v-if="now_list && now_list.length"
       :now_list='now_list'
       :sort_items='sort_items'
+      :order='order'
     />
-    <Modal>
-      <div slot='body' class="modal-body">
-        <button
-          type='button'
-          class='close'
-          data-dismiss="modal"
-          aria-label="Close"
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <Loan ref="loan"/>
+    <div
+      class="pagination-nav"
+      v-bind:class="{ 'show': now_list && now_list.length }"
+    >
+      <b-pagination
+        :total-rows="data.total"
+        v-model="data.current_page"
+        :per-page="Number(data.per_page)"
+        @input='change_page'
+      />
+      <div class='total-page'>
+        <span>共 {{data.last_page}} 頁</span>
       </div>
-    </Modal>
+      <div class="go-to-page">
+        <span>到</span>
+        <input ref='go-to-input' type='text' class='form-control'/>
+        <span>頁</span>
+        <b-button
+          variant='outline-primary'
+          @click='handle_go_to_page_click'
+        >換頁</b-button>
+      </div>
+      <div class='per-page'>
+        <span>每頁顯示</span>
+        <b-form-select
+          v-model='data.per_page'
+          :value="data.per_page"
+          :options="{ '2': 2, '5': 5, '10': 10, '20': 20 }"
+          @change='change_pre_page'
+        />
+        <span>筆</span>
+      </div>
+    </div>
+    <div class='modal fade' id='modal' ref='modal'>
+      <div class='modal-dialog'>
+        <div class='modal-content'>
+          <div slot='body' class="modal-body">
+            <button
+              type='button'
+              class='close'
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <Loan ref="loan"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,8 +67,11 @@
     set_expiration
   } from '../util'
   import List from './List'
-  import Modal from './Modal'
   import Loan from './Loan'
+  import bPagination from 'bootstrap-vue/es/components/pagination/pagination'
+  import bFormInput from 'bootstrap-vue/es/components/form-input/form-input'
+  import bButton from 'bootstrap-vue/es/components/button/button'
+  import bFormSelect from 'bootstrap-vue/es/components/form-select/form-select'
 
   export default {
     data () {
@@ -39,28 +80,41 @@
         now_list: {},
         now_item: {},
         sort_rule: null,
+        sort_reverse: false,
         sorted_items: {},
         errors: []
       }
     },
     created () {
-      axios
-        .get('//localhost:9090/market-place')
-        .then(res => res.data)
-        .then(market_place => {
-          this.data = market_place.data
-          this.now_list = this.create_now_list(this.classify_items(this.data.data))
-          this.now_list.map(item => {
-            set_expiration(item)
-          })
-        })
-        .catch(e => {
-          this.errors.push(e)
-        })
+      this.get_data()
     },
     mounted () {
       $(this.$refs.modal).on('hidden.bs.modal', () => {
         this.$router.push('/market-place')
+        $(window).off('keydown.hideModal')
+      })
+
+      $(this.$refs.modal).on('shown.bs.modal', () => {
+        $(window).on('keydown.hideModal', e => {
+          if (e.keyCode === 27) {
+            $(this.$refs.modal).modal('hide')
+          }
+        })
+      })
+
+      $(this.$refs['go-to-input']).on('focus', e => {
+        console.log('focus')
+        const $target = $(e.target)
+        console.log($target)
+        $target.on('keydown', e => {
+          if (e.keyCode === 13) {
+            this.go_to_page_change(Number($target[0].value))
+          }
+        })
+
+        $target.on('blur', () => {
+          $target.off('keydown')
+        })
       })
     },
     beforeRouteUpdate (to, from, next) {
@@ -74,6 +128,41 @@
       }
     },
     methods: {
+      get_data: function (page, per_page) {
+        axios
+          .get('//localhost:9090/market-place', {
+            page: page,
+            perPage: per_page
+          })
+          .then(res => res.data)
+          .then(market_place => {
+            const data = market_place.data
+            let classify_items
+
+            if (page) {
+              data.current_page = page
+            }
+            if (per_page) {
+              data.per_page = per_page
+            }
+
+            this.data = data
+            classify_items = this.classify_items(data.data)
+
+            if (this.sort_rule) {
+              this.sort_items(this.sort_rule)
+            } else {
+              this.now_list = this.create_now_list(classify_items)
+            }
+
+            this.now_list.map(item => {
+              set_expiration(item)
+            })
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      },
       set_now_item: function (serial) {
         const item_data = search_data(
           'serial',
@@ -98,20 +187,24 @@
           expired: classify('expired')
         }
       },
-      sort_items: function (rule) {
+      sort_items: function (e) {
         const data_items =
           !!this.sorted_items['on_sale']
           ? this.sorted_items
           : this.classify_items(this.data.data)
         let { on_sale, backed, expired } = data_items
 
-        const reverse = () => {
+        const reverse = (is_change_page) => {
           on_sale = on_sale.reverse()
           backed = backed.reverse()
           expired = expired.reverse()
+
+          if (!is_change_page) {
+            this.sort_reverse = !this.sort_reverse
+          }
         }
 
-        const sort = () => {
+        const sort = rule => {
           const valuePath = (() => {
             let testItem
             if (on_sale[0]) {
@@ -155,25 +248,34 @@
           expired = sort_array(expired, rule)
         }
 
-        const set_class = () => {
-          const $el = $(this.$refs[rule])
-          if ($el.hasClass('sort-down')) {
-            $el.removeClass('sort-down').addClass('sort-up')
-          } else {
-            $el.removeClass('sort-up').addClass('sort-down')
-          }
-          $el.siblings().removeClass('sort-down sort-up')
-        }
-
-        if (rule === this.sort_rule) {
-          reverse()
+        if (typeof e === 'string') {
+          sort(e)
+          this.sort_reverse && reverse(true)
         } else {
-          sort()
+          const $target = $(e.currentTarget)
+          const rule = $target.data('sort')
+
+          const set_class = () => {
+            if ($target.hasClass('sort-down')) {
+              $target.removeClass('sort-down').addClass('sort-up')
+            } else {
+              $target.removeClass('sort-up').addClass('sort-down')
+            }
+            $target.siblings().removeClass('sort-down sort-up')
+          }
+
+          if (rule === this.sort_rule) {
+            reverse()
+          } else {
+            sort(rule)
+            this.sort_reverse = false
+          }
+
+          set_class()
+
+          this.sort_rule = rule
         }
 
-        set_class()
-
-        this.sort_rule = rule
         this.sorted_items = { on_sale, backed, expired }
         this.now_list = this.create_now_list(this.sorted_items)
       },
@@ -192,12 +294,49 @@
       },
       order: function (serial) {
         window.alert(serial)
+      },
+      change_page: function (page) {
+        // here is a bug of this.data.per_page
+        // when change_pre_page pass new value larger then current
+        // this.data.pre_page will be in string type
+        // it will comes a error message in b-pagination component
+        // so now, I translate the type to number for avoid this bug
+        // it should be fixed
+        // - whitefur
+        this.get_data(page, this.data.per_page)
+      },
+      change_pre_page: function (per_page) {
+        per_page = Number(per_page)
+        let page
+
+        if (this.data.current_page * per_page > this.data.total) {
+          page = Math.ceil(this.data.total / per_page)
+        } else {
+          page = this.data.current_page
+        }
+
+        this.get_data(page, per_page)
+      },
+      handle_go_to_page_click: function () {
+        this.go_to_page_change(this.$refs['go-to-input'].value)
+      },
+      go_to_page_change: function (page) {
+        page = Number(page)
+        if (page <= this.data.last_page && page >= 1) {
+          this.change_page(Number(page))
+          this.$refs['go-to-input'].value = null
+        } else {
+          window.alert('輸入的頁數不正確')
+        }
       }
     },
     components: {
       Loan,
-      Modal,
-      List
+      List,
+      'b-pagination': bPagination,
+      'b-form-input': bFormInput,
+      'b-button': bButton,
+      'b-form-select': bFormSelect
     }
   }
 </script>
